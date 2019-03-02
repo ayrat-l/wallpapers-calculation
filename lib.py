566ee6d77677d315cab07f94fbdf915833dd698f
ayrat-l <kaz90@bk.ru>
2019-03-02 11:12:23 +0300

    Refactoring code

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 import math
 
 def wallpapers_calc(width_room,lenght_room,height_room,width_wallpapers,lenght_wallpapers):
+    stock = 0.10 # Запасные 10 см к длине обоев
     """
     >>> wallpapers_calc(3,6,2.6,1.4,10)
     4
@@ -12,8 +13,8 @@ def wallpapers_calc(width_room,lenght_room,height_room,width_wallpapers,lenght_w
     #Узнаем количество полотнищ
     count_canvas = math.ceil(perimeter_room / width_wallpapers)
 
-    #Из одного рулона выйдет __ полотна (+10 см запасные)
-    count_canvas_in_1_wallpapers = math.floor((lenght_wallpapers / (height_room + 0.10)))
+    #Из одного рулона выйдет __ полотна
+    count_canvas_in_1_wallpapers = math.floor(lenght_wallpapers / (height_room + stock))
 
     #Обоев всего понадобится
     wallpapers_calc = count_wallpapers = math.floor(count_canvas /count_canvas_in_1_wallpapers)
